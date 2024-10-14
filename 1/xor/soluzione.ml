33cfec2e62565ebfc45f12ee0830a2475a728bf9
giuliaacannata05 <g.cannata4@studenti.unica.it>
2024-10-14 12:13:20 +0200

    soluzioneXor

--- 1/xor/soluzione.ml
+++ 1/xor/soluzione.ml
@@ -1,9 +1,9 @@
-let nand1 (a : bool) (b : bool) : bool = (not(a &&b)) && ((a || b))
+let xor1 (a : bool) (b : bool) : bool = (not(a &&b)) && ((a || b))
 
 
-let nand2 (a : bool) (b : bool) : bool = if a then (if b then false else true) else (if b then true else false )
+let xor2 (a : bool) (b : bool) : bool = if a then (if b then false else true) else (if b then true else false )
 
-let nand2 (a : bool) (b : bool) : bool = 
+let xor3 (a : bool) (b : bool) : bool = 
     match (a,b) with 
     | (true, true) -> false
     | (false, false) -> false
